eb9ce6648af67552fc49621c78e93827b0429dcf
gabscar <srgabsc@gmail.com>
2021-09-27 11:45:20 -0300

    feat: adquirindo permissão

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
+  
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location'
 
 export default function App() {
+  const [error, setError] = useState(null)
+  useEffect(() => {
+    Load()
+  }, [])
+
+  const Load = async() => {
+    try{
+      let { status } = await Location.requestBackgroundPermissionsAsync()
+
+      if (status !== 'granted') {
+        setError('Acess to location is needed')
+        return
+      }
+      const location = await Location.getCurrentPositionAsync()
+      const { latitude, longitude } = location.coords
+      alert(`${latitude} ${longitude}`)
+    }catch(error){}
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +34,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'white',
     alignItems: 'center',
     justifyContent: 'center',
   },
