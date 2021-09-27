da38277e74ee8662a46e4c689dce00706c90e412
gabscar <srgabsc@gmail.com>
2021-09-27 14:24:52 -0300

    connect to api location

--- App.js
+++ App.js
@@ -3,32 +3,70 @@ import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import * as Location from 'expo-location'
+import GetLocation from 'react-native-get-location'
 
+const BASE_API_KEY = '431418e1b338e897140df877824108ee';
+
+const URL = 'https://api.openweathermap.org/data/2.5/weather?'
 export default function App() {
-  const [error, setError] = useState(null)
+  const [error, setError] = useState(null);
+  const [latitude,setLatitude] = useState(null)
+  const [wheater, setWheater] = useState([]);
   useEffect(() => {
-    Load()
+   Load();
+  
   }, [])
 
-  const Load = async() => {
+  async function Load(){
     try{
-      let { status } = await Location.requestBackgroundPermissionsAsync()
+      let { status } = await Location.requestForegroundPermissionsAsync();
 
       if (status !== 'granted') {
         setError('Acess to location is needed')
         return
       }
-      const location = await Location.getCurrentPositionAsync()
-      const { latitude, longitude } = location.coords
-      alert(`${latitude} ${longitude}`)
-    }catch(error){}
+      let location = await Location.getCurrentPositionAsync({});
+
+      const { coords } = location;
+      
+     
+      const { latitude, longitude } = coords;
+      setLatitude(latitude)
+      const wheatherURL = `${URL}lat=${latitude}&lon=${longitude}&appid=${BASE_API_KEY}`
+      const response = await fetch(wheatherURL);
+      const result = await response.json();
+      
+      
+      
+      
+      if(response.ok) {
+        setWheater(result)
+      } else {
+        setError(result.message)
+      }
+
+    }catch(error){
+      setErrorMessage(error.message)
+    }
   }
-  return (
-    <View style={styles.container}>
-      <Text>Hello world</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  if(wheater) {
+    const { main } = wheater
+    return (
+      <View style={styles.container}>
+        <Text>{latitude}</Text>
+        <StatusBar style="auto" />
+      </View>
+    )
+  }else if (error) {
+    return (
+      <View style={styles.container}>
+          
+          <Text style={{ textAlign: 'center' }}>{error}</Text>
+          
+      </View>
+    )
+  }
+;
 }
 
 const styles = StyleSheet.create({
