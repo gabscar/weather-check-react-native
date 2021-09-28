7d7faa8a1901336e13fb574a11e745e68d7d22c5
gabscar <srgabsc@gmail.com>
2021-09-28 07:43:11 -0300

    fix: correções de bug

--- App.js
+++ App.js
@@ -1,85 +1,90 @@
 
 import { StatusBar } from 'expo-status-bar';
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,ActivityIndicator } from 'react-native';
 import * as Location from 'expo-location'
-import WheatherInfo from './components/WheatherInfo';
+import WeatherInfo from './components/WeatherInfo';
 import ReloadIcon from './components/ReloadIcon';
 import Picker from './components/Picker';
 import WeatherDetails from './components/WeatherDetails';
-
+import { colors } from './utils/index'
 const BASE_API_KEY = '431418e1b338e897140df877824108ee';
 
-const URL = 'https://api.openweathermap.org/data/2.5/weather?'
+const URL = 'https://api.openweathermap.org/data/2.5/weather?';
+
 export default function App() {
-  const [error, setError] = useState(null)
-  const [currentWeather, setWheater] = useState([])
-  const [units, setUnits] = useState('metric')
+  const {PRIMARY_COLOR} = colors;
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [currentWeather, setCurrentWeather] = useState(null);
+  const [units, setUnits] = useState("metric");
   useEffect(() => {
-    Load()
-  }, [units])
+    load();
+  }, [units]);
 
-  const Load = async() => {
-    try{
-      let { status } = await Location.requestBackgroundPermissionsAsync()
-
-      if (status !== 'granted') {
-        setError('Acess to location is needed')
-        return
+  const load = async () => {
+    setCurrentWeather(null)
+    setErrorMessage(null)
+    try {
+      let { status } = await Location.requestBackgroundPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMessage("Access to location is needed to run the app");
+        return;
       }
-      const location = await Location.getCurrentPositionAsync( {  accuracy: Location.Accuracy.High })
-      const { latitude, longitude } = location.coords
-      const wheatherURL = `${URL}lat=${latitude}&lon=${longitude}&appid=${BASE_API_KEY}&units=${units}`
-      const response = await fetch(wheatherURL)
-      const result = await response.json()
-      if(response.ok) {
-        setWheater(result)
+      const location = await Location.getCurrentPositionAsync();
+      const { latitude, longitude } = location.coords;
+      const weatherUrl = `${URL}lat=${latitude}&lon=${longitude}&units=${units}&appid=${BASE_API_KEY}`;
+      const response = await fetch(weatherUrl);
+      const result = await response.json();
+      if (response.ok) {
+        setCurrentWeather(result);
       } else {
-        setError(result.message)
+        setErrorMessage(result.message);
       }
-    }catch(error){
-      setError(error.message)
+    } catch (error) {
+      setErrorMessage(error.message);
     }
-  }
-  if(currentWeather !== undefined) {
-    const { weather, name } = currentWeather
-      return (
-        <View style={styles.container}>      
-          <StatusBar style="auto" />
-          <View style={styles.main}>
-          <Picker unitSystem={units} setUnits={setUnits}/>
-            <ReloadIcon load={Load}/>
-            <WheatherInfo currentWeather={currentWeather} weather={weather} name={name}/>
-            <WeatherDetails currentWeather={currentWeather} metricSystem={units}/>
-          </View>
+  };
 
-        </View>
-      )
-  }else if (error){
-            <View style={styles.container}>
-                <Text style={{ textAlign: 'center' }}>{errorMessage}</Text>
-                <StatusBar style="auto" />
-            </View>
-  }else{
+  if (currentWeather) {
     return (
       <View style={styles.container}>
-          <ActivityIndicator size="large" color={colors.PRIMARY_COLOR} />
-          <StatusBar style="auto" />
+        <StatusBar style="auto" />
+        <View style={styles.main}>
+          <Picker
+            units={units}
+            setUnits={setUnits}
+          />
+          <ReloadIcon load={load} />
+          <WeatherInfo currentWeather={currentWeather} />
+        </View>
+        <WeatherDetails currentWeather={currentWeather} units={units}/>
       </View>
-    )
+    );
+  } else if (errorMessage) {
+    return (
+      <View style={styles.container}>
+        <ReloadIcon load={load}/>
+        <Text style={{textAlign:'center'}}>{errorMessage}</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  } else {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator size='large' color={PRIMARY_COLOR} />
+        <StatusBar style="auto" />
+      </View>
+    );
   }
-;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
   },
   main: {
-    justifyContent: 'center',
-    flex: 1
-  }
+    flex: 1,
+    justifyContent: "center",
+  },
 });
--- components/WeatherDetails.js
+++ components/WeatherDetails.js
@@ -1,93 +1,122 @@
-import React, { useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { colors } from '../utils/index'
-const { PRIMARY_COLOR, SECONDARY_COLOR, BORDER_COLOR } = colors
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { colors } from "../utils/index";
+import { FontAwesome5, MaterialCommunityIcons } from "@expo/vector-icons";
 
-export default function WeatherDetails({ currentWeather, unitsSystem }) {
-    const [feels_like, setFeels_like] = useState('Loading')
-    const [humidity, setHumidity] = useState('loading')
-    const [speed, setSpeed] = useState('loading')
-    const [pressure, setPressure] = useState('loading')
+const { PRIMARY_COLOR, SECONDARY_COLOR, BORDER_COLOR } = colors;
 
-    if(currentWeather) {
-    const {
-        main: { feels_like, humidity, pressure },
-        wind: { speed },
-    } = currentWeather
-    setFeels_like(feels_like);
-    setHumidity(humidity)
-    setSpeed(speed)
-    setPressure(pressure)
-}
-    const windSpeed = unitsSystem === 'metric' ? `${Math.round(speed)} m/s` : `${Math.round(speed)} miles/h`
-       return (<View style={styles.weatherDetails}>
-            <View style={styles.weatherDetailsRow}>
-                <View style={{ ...styles.weatherDetailsBox, borderRightWidth: 1, borderRightColor: BORDER_COLOR }}>
-                    <View style={styles.weatherDetailsRow}>
-                        <View style={styles.weatherDetailsItems}>
-                            <Text>Feels like :</Text>
-                            <Text style={styles.textSecondary}>{feels_like} °</Text>
-                        </View>
-                    </View>
-                </View>
-                <View style={styles.weatherDetailsBox}>
-                    <View style={styles.weatherDetailsRow}>
-                        <MaterialCommunityIcons name="water" size={30} color={PRIMARY_COLOR} />
-                        <View style={styles.weatherDetailsItems}>
-                            <Text>Humidity :</Text>
-                            <Text style={styles.textSecondary}>{humidity} %</Text>
-                        </View>
-                    </View>
-                </View>
+export default function WeatherData({ currentWeather, unitsSystem }) {
+  const windSpeed =
+    unitsSystem === "metric"
+      ? `${Math.round(currentWeather.wind.speed)} m/s`
+      : `${Math.round(currentWeather.wind.speed)} miles/h`;
+  return (
+    <View style={styles.weatherDetails}>
+      <View style={styles.weatherDetailsRow}>
+        <View
+          style={{
+            ...styles.weatherDetailsBox,
+            borderRightWidth: 1,
+            borderRightColor: BORDER_COLOR,
+          }}
+        >
+          <View style={styles.weatherDetailsRow}>
+            <FontAwesome5
+              name="temperature-low"
+              size={25}
+              color={PRIMARY_COLOR}
+            />
+            <View style={styles.weatherDetailsItems}>
+              <Text>Feels Like: </Text>
+              <Text style={styles.textSecondary}>
+                {currentWeather.main.feels_like}º
+              </Text>
             </View>
-            <View style={{ ...styles.weatherDetailsRow, borderTopWidth: 1, borderTopColor: BORDER_COLOR }}>
-                <View style={{ ...styles.weatherDetailsBox, borderRightWidth: 1, borderRightColor: BORDER_COLOR }}>
-                    <View style={styles.weatherDetailsRow}>
-                        <MaterialCommunityIcons name="weather-windy" size={30} color={PRIMARY_COLOR} />
-                        <View style={styles.weatherDetailsItems}>
-                            <Text>Wind Speed :</Text>
-                            <Text style={styles.textSecondary}>{windSpeed}</Text>
-                        </View>
-                    </View>
-                </View>
-                <View style={styles.weatherDetailsBox}>
-                    <View style={styles.weatherDetailsRow}>
-                        <MaterialCommunityIcons name="speedometer" size={30} color={PRIMARY_COLOR} />
-                        <View style={styles.weatherDetailsItems}>
-                            <Text>Pressure :</Text>
-                            <Text style={styles.textSecondary}>{pressure} hPa</Text>
-                        </View>
-                    </View>
-                </View>
+          </View>
+        </View>
+        <View style={styles.weatherDetailsBox}>
+          <View style={styles.weatherDetailsRow}>
+            <MaterialCommunityIcons
+              name="water"
+              size={30}
+              color={PRIMARY_COLOR}
+            />
+            <View style={styles.weatherDetailsItems}>
+              <Text>Humidity: </Text>
+              <Text style={styles.textSecondary}>
+                {currentWeather.main.humidity}%
+              </Text>
             </View>
-        </View>)
+          </View>
+        </View>
+      </View>
+      <View style={{...styles.weatherDetailsRow,borderTopWidth:1,borderTopColor:BORDER_COLOR}}>
+        <View
+          style={{
+            ...styles.weatherDetailsBox,
+            borderRightWidth: 1,
+            borderRightColor: BORDER_COLOR,
+          }}
+        >
+          <View style={styles.weatherDetailsRow}>
+            <FontAwesome5
+              name="wind"
+              size={25}
+              color={PRIMARY_COLOR}
+            />
+            <View style={styles.weatherDetailsItems}>
+              <Text>Wind Speed: </Text>
+              <Text style={styles.textSecondary}>
+                {windSpeed}
+              </Text>
+            </View>
+          </View>
+        </View>
+        <View style={styles.weatherDetailsBox}>
+          <View style={styles.weatherDetailsRow}>
+            <MaterialCommunityIcons
+              name="speedometer"
+              size={30}
+              color={PRIMARY_COLOR}
+            />
+            <View style={styles.weatherDetailsItems}>
+              <Text>Pressure: </Text>
+              <Text style={styles.textSecondary}>
+                {currentWeather.main.pressure} hPa
+              </Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    weatherDetails: {
-        marginTop: 'auto',
-        margin: 15,
-        borderWidth: 1,
-        borderColor: BORDER_COLOR,
-        borderRadius: 10,
-    },
-    weatherDetailsRow: {
-        flexDirection: 'row',
-        alignItems: 'center',
-        justifyContent: 'space-between',
-    },
-    weatherDetailsBox: {
-        flex: 1,
-        padding: 20,
-    },
-    weatherDetailsItems: {
-        alignItems: 'flex-end',
-        justifyContent: 'flex-end',
-    },
-    textSecondary: {
-        fontSize: 15,
-        color: SECONDARY_COLOR,
-        fontWeight: '700',
-        margin: 7,
-    },
-})
+  weatherDetails: {
+    marginTop: "auto",
+    borderWidth: 1,
+    margin: 15,
+    borderColor: BORDER_COLOR,
+    borderRadius: 10,
+  },
+  weatherDetailsRow: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+  },
+  weatherDetailsBox: {
+    flex: 1,
+    padding: 20,
+  },
+  weatherDetailsItems: {
+    alignItems: "flex-end",
+    justifyContent: "flex-end",
+  },
+  textSecondary: {
+    fontSize: 15,
+    color: SECONDARY_COLOR,
+    fontWeight: "700",
+    margin: 7,
+  },
+});
